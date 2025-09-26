fa08e115012ec280e6794258a101307588a38783
Рустем <rustem@MacBook-Air-Rustem.local>
2025-09-26 21:06:30 +0300

    Debag

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 const button = document.getElementById("button")
+const navig = document.querySelector(".task__header__nav-item")
+const fotter = document.querySelector(".task__footer-div-list")
 
 button.addEventListener("click", (event) => {
   event.preventDefault()
-  return event.target
 })
+
+navig.addEventListener("click", (event) => {
+  event.target.scrollIntoView({
+    behavior: "smooth",
+  })
+})
+
+fotter.addEventListener("click", (event) => {
+  const item = event.target
+  if(item) copyText(item);
+  return
+})
+
+function copyText(text) {
+
+}
