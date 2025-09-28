f7cbbd8787e6a003a76a8b61c76fe51f27834ca1
Рустем <rustem@MacBook-Air-Rustem.local>
2025-09-28 18:32:05 +0300

    js fetch

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const navig = document.querySelector(".task__header__nav-item")
 const fotter = document.querySelector(".task__footer-div-list")
+const form1 = document.getElementById("task__menager-form")
+const form2 = document.getElementById("task__article-form")
 
 navig.addEventListener("click", () => {
   fotter.scrollIntoView({
@@ -19,3 +21,53 @@ document
   })
   .then((mes) => console.log(`you copied ${mes}`))
   .catch((e) => console.error("error:", e))
+
+form1.addEventListener("click", (e) => {
+  sendRequest(e)
+})
+
+form2.addEventListener("click", (e) => {
+  sendRequest(e)
+})
+
+async function sendRequest(event) {
+  event.preventDefault()
+  const formData = new FormData(form1)
+  const formData2 = new FormData(form2)
+  const inputText = formData.get("input1")
+  const inputText2 = formData2.get("input2")
+
+  if (inputText.length > 1000) {
+    form1.innerText = "Запрос слишком длинный"
+    return
+  }
+  if (!inputText && !inputText2) {
+    form1.textContent = "Please, write prompt"
+    form2.textContent = "Write your solution"
+    return
+  }
+
+  form1.textContent = "Loading..."
+
+  try {
+    const response = await fetch("/api/neural/query", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ input1: inputText, input2: inputText2 }),
+    })
+
+    const data = await response.json()
+
+    if (response.ok) {
+      form1.textContent = data.result
+      form2.textContent = data.result
+    }
+    form1.textContent = `Error: ${data.message || "Error"}`
+    form2.textContent = `Error: ${data.message || "Error"}`
+  } catch (err) {
+    form1.textContent = `Error: ${err.message}`
+    form2.textContent = `Error: ${err.message}`
+  }
+}
