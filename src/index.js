a24cc08d90dc13bae4a4d0b708953eaca274655c
Рустем <rustem@MacBook-Air-Rustem.local>
2025-09-28 11:19:10 +0300

    env.example

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
-const button = document.getElementById("button")
 const navig = document.querySelector(".task__header__nav-item")
 const fotter = document.querySelector(".task__footer-div-list")
-
-button.addEventListener("click", (event) => {
-  event.preventDefault()
-})
 
 navig.addEventListener("click", () => {
   fotter.scrollIntoView({
