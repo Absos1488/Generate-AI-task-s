1158ef7f44ee31b64c754f7ad4bb2dbb3a66e8ca
Рустем <rustem@MacBook-Air-Rustem.local>
2025-09-28 19:45:46 +0300

    Add hardest

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const navig = document.querySelector(".task__header__nav-item")
 const fotter = document.querySelector(".task__footer-div-list")
 const form1 = document.getElementById("task__menager-form")
 const form2 = document.getElementById("task__article-form")
+const harder = document.getElementById("harder")
 
 navig.addEventListener("click", () => {
   fotter.scrollIntoView({
@@ -36,6 +37,7 @@ async function sendRequest(event) {
   const formData2 = new FormData(form2)
   const inputText = formData.get("input1")
   const inputText2 = formData2.get("input2")
+  const harders = harder.value
 
   if (inputText.length > 1000) {
     form1.textContent = "Запрос слишком длинный"
@@ -55,7 +57,11 @@ async function sendRequest(event) {
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ input1: inputText, input2: inputText2 }),
+      body: JSON.stringify({
+        input1: inputText,
+        input2: inputText2,
+        hard: harders,
+      }),
     })
 
     const data = await response.json()
